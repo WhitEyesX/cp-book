3fd8ac743861356e009bf09243dc868cc63a39b5
Daniil Beloglazov <whiteyesx205@gmail.com>
2023-05-25 21:30:19 +0300

    VeryEasy Math USACO

---
+++ Reading Books/Reading Books.cpp
@@ -0,0 +1,29 @@
+﻿/*
+* There are n books, and Kotivalo and Justiina are going to read them all. For each book, you know the time it takes to read it.
+*
+* They both read each book from beginning to end, and they cannot read a book at the same time. What is the minimum total time required?
+*
+* The first input line has an integer n: the number of books.
+* The second line has n integers t1,t2,…,tn: the time required to read each book.
+*
+* Print one integer: the minimum total time.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    long long total = 0, maxEl = 0;
+    while (n--) {
+        long long t;
+        cin >> t;
+        total += t;
+        maxEl = max(maxEl, t);
+    }
+
+    cout << max(total, maxEl * 2);
+}
---
+++ Who's Opposite/Who's Opposite.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int solve(int a, int b, int c) {
+    if (a > b) swap(a, b);
+    int preA = a - 1, range = b - a, len = range * 2;
+    if (preA > range - 1 || c > len) return -1;
+    return (c + range) / len + (c + range) % len; 
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << "Ans: " << solve(a, b, c) << "\n";
+    }
+}
+
